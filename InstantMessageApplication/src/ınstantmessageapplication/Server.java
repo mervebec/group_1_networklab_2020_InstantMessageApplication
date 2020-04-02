31ec4a316611245648d9ebd0d21cbb77aa8628b2
MERVE BECERIKLI <mervebec@gmail.com>
2020-04-03 00:16:48 +0300

    revision - 1

--- InstantMessageApplication/src/ınstantmessageapplication/Server.java
+++ InstantMessageApplication/src/ınstantmessageapplication/Server.java
@@ -1,14 +1,154 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ınstantmessageapplication;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author BECERIKLI
  */
 public class Server {
-    
+
+    private ServerSocket serverSocket;
+    private javax.swing.JTextPane historyJTextPane;
+    private Thread serverThread;
+    private HashSet<ObjectOutputStream> allContacts = new HashSet<>();
+
+    protected void sendMessage(String message) throws IOException {
+        // buna bakacoğuz..
+
+    }
+
+    protected void start(int port) throws IOException {
+        // server soketi oluşturma (sadece port numarası)
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server başlatıldı ..");
+
+        // server arayüzündeki history alanı, bütün olaylar buraya yazılacak
+        // arayüzü kitlememek için, server yeni client bağlantılarını ayrı Thread'de beklemeli
+        serverThread = new Thread(() -> {
+            while (!serverSocket.isClosed()) {
+                try {
+                    // blocking call, yeni bir client bağlantısı bekler
+                    Socket contactSocket = serverSocket.accept();
+                    System.out.println("Yeni bir kullanıcı bağlandı : " + contactSocket);
+
+                    // bağlanan her client için bir thread oluşturup dinlemeyi başlat
+                    new ListenThread(contactSocket).start();
+                } catch (IOException ex) {
+                    System.out.println("Hata - new Thread() : " + ex);
+                    break;
+                }
+            }
+        });
+        serverThread.start();
+    }
+
+    protected void stop() throws IOException {
+        // bütün streamleri ve soketleri kapat
+        if (serverSocket != null) {
+            serverSocket.close();
+        }
+        if (serverThread != null) {
+            serverThread.interrupt();
+        }
+    }
+
+    class ListenThread extends Thread {
+
+        // dinleyeceğimiz client'ın soket nesnesi, input ve output stream'leri
+        private final Socket contactSocket;
+        private ObjectInputStream contactInput;
+        private ObjectOutputStream contactOutput;
+
+        private ListenThread(Socket contactSocket) {
+            this.contactSocket = contactSocket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Bağlanan client için thread oluşturuldu : " + this.getName());
+
+            try {
+                // input  : client'dan gelen mesajları okumak için
+                // output : server'a bağlı olan client'a mesaj göndermek için
+                contactInput = new ObjectInputStream(contactSocket.getInputStream());
+                contactOutput = new ObjectOutputStream(contactSocket.getOutputStream());
+
+                // Bütün client'lara yeni katılan client bilgisini gönderir
+                for (ObjectOutputStream out : allContacts) {
+                    out.writeObject(this.getName() + " server'a katıldı.");
+                }
+
+                // broadcast için, yeni gelen client'ın output stream'ını listeye ekler
+                allContacts.add(contactOutput);
+
+                // client ismini mesaj olarak gönder
+                contactOutput.writeObject("@id-" + this.getName());
+
+                Object mesaj;
+                // client mesaj gönderdiği sürece mesajı al
+                while ((mesaj = contactInput.readObject()) != null) {
+                    // client'in gönderdiği mesajı server ekranına yaz
+                    System.out.println(this.getName() + " : " + mesaj);
+
+                    // bütün client'lara gelen bu mesajı gönder
+                    for (ObjectOutputStream out : allContacts) {
+                        out.writeObject(this.getName() + ": " + mesaj);
+                    }
+
+                    // "son" mesajı iletişimi sonlandırır
+                    if (mesaj.equals("son")) {
+                        break;
+                    }
+                }
+
+            } catch (IOException | ClassNotFoundException ex) {
+                System.out.println("Hata - ListenThread : " + ex);
+            } finally {
+                try {
+                    // client'ların tutulduğu listeden çıkart
+                    allContacts.remove(contactOutput);
+
+                    // bütün client'lara ayrılma mesajı gönder
+                    for (ObjectOutputStream out : allContacts) {
+                        out.writeObject(this.getName() + " server'dan ayrıldı.");
+                    }
+
+                    // bütün streamleri ve soketleri kapat
+                    if (contactInput != null) {
+                        contactInput.close();
+                    }
+                    if (contactOutput != null) {
+                        contactOutput.close();
+                    }
+                    if (contactSocket != null) {
+                        contactSocket.close();
+                    }
+                    System.out.println("Soket kapatıldı : " + contactSocket);
+                } catch (IOException ex) {
+                    System.out.println("Hata - Soket kapatılamadı : " + ex);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server;
+        
+        int port = 11111;
+        server = new Server();
+        
+        try {
+            server.start(port);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
---
+++ InstantMessageApplication/src/ınstantmessageapplication/Welcome_UI.java
@@ -0,0 +1,136 @@
+package ınstantmessageapplication;
+
+/**
+ *
+ * @author BECERIKLI
+ */
+public class Welcome_UI extends javax.swing.JFrame {
+    
+    Contact contact;
+    private String localhost;
+    
+    public Welcome_UI() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        btn_giris = new javax.swing.JButton();
+        txt_phoneNumber = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txt_name = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Oturum Açma");
+
+        btn_giris.setText("GİRİŞ YAP");
+        btn_giris.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_girisActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Telefon Numarası");
+
+        jLabel3.setText("İsim Soyisim");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 131, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addComponent(txt_phoneNumber)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txt_name)
+                    .addComponent(btn_giris, javax.swing.GroupLayout.DEFAULT_SIZE, 217, Short.MAX_VALUE))
+                .addContainerGap(30, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(53, 53, 53)
+                .addComponent(jLabel1)
+                .addGap(29, 29, 29)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(txt_phoneNumber, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(txt_name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40)
+                .addComponent(btn_giris)
+                .addContainerGap(118, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_girisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_girisActionPerformed
+        
+        try {
+            String host = localhost;
+            int port = 11111;
+            contact = new Contact();
+            contact.start(host, port, txt_phoneNumber, txt_name);
+           
+        } catch (Exception ex) {
+            System.out.println("Error - jButtonConnectActionPerformed : " + ex);
+            
+        }
+        
+    }//GEN-LAST:event_btn_girisActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Welcome_UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Welcome_UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Welcome_UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Welcome_UI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Welcome_UI().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_giris;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField txt_name;
+    private javax.swing.JTextField txt_phoneNumber;
+    // End of variables declaration//GEN-END:variables
+}
